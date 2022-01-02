d9cf4867bba2cde40b0f2ff07a5bc3d16818b979
Amin Zayeromali <amin.zayeromali@gmail.com>
2022-01-02 16:59:52 +0330

    New Codes & Projects Added ...

---
+++ University-Projects/Find-S-Algorithms-Implementation.py
@@ -0,0 +1,46 @@
+import pandas as pd
+import numpy as np
+
+# to read the data in the csv file or Create Data frame
+# ---------------- Read File Codes ------------
+# data = pd.read_csv("data.csv")
+# print(data, "n")
+# ----------- Create DataFrame Codes ----------
+data = pd.DataFrame([['Morning', 'Sunny', 'Warm', 'Yes', 'Mild', 'Strong', 'Yes'],
+                     ['Evening', 'Rainy', 'Cold', 'No', 'Mild', 'Normal', 'No'],
+                     ['Morning', 'Sunny', 'Moderate',
+                         'Yes', 'Normal', 'Normal', 'Yes'],
+                     ['Evening', 'Sunny', 'Cold', 'Yes', 'High', 'Strong', 'Yes'],
+                     ['Morning', 'Sunny', 'Moderate', 'Yes', 'Mild', 'Strong', 'Yes']],
+                    columns=['Time', 'Weather', 'Temperature', 'Company', 'Humidity', 'Wind', 'Goes'])
+
+# making an array of all the attributes
+d = np.array(data)[:, :-1]
+print("n The attributes are: ", d)
+
+# segragating the target that has positive and negative examples
+target = np.array(data)[:, -1]
+print("n The target is: ", target)
+
+# training function to implement find-s algorithm
+
+
+def train(c, t):
+    for i, val in enumerate(t):
+        if val == "Yes":
+            specific_hypothesis = c[i].copy()
+            break
+
+    for i, val in enumerate(c):
+        if t[i] == "Yes":
+            for x in range(len(specific_hypothesis)):
+                if val[x] != specific_hypothesis[x]:
+                    specific_hypothesis[x] = '?'
+                else:
+                    pass
+
+    return specific_hypothesis
+
+
+# obtaining the final hypothesis
+print("n The final hypothesis is:", train(d, target))
