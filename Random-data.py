c0489cb34e9f14cc1c50b831ebdec6228341385d
Amin Zayeromali <amin.zayeromali@gmail.com>
2022-01-13 13:12:45 +0330

    New Update  ...

--- Random-data.py
+++ Random-data.py
@@ -1,3 +1,5 @@
+import matplotlib.pyplot as plt
+from math import pi, cos, sin
 from random import random
 from random import seed
 
@@ -10,7 +12,7 @@ print("\nPrint 1D Random Numbers")
 for i in range(100):
     print("[",a + (b-a) * random(),"]")
 
-# 2D Random Number Between [3,7] and [] 
+# 2D Random Number Between [3,7] and [2,6] 
 a = 3
 b = 7
 c = 2
@@ -20,4 +22,17 @@ print("\nPrint 2D Random Numbers")
 for i in range(100):
     print("[",a + (b-a) * random(),"],[",c+ (d-c)* random(),"]")
 
+# Generating random points on the circumference of a circle
+def point(h, k, r):
+    theta = random() * 2 * pi
+    return h + cos(theta) * r, k + sin(theta) * r
 
+
+xy = [point(1, 2, 1) for _ in range(30)]
+print(xy)
+
+# Show Points
+plt.scatter(*zip(*xy))
+plt.grid(color='k', linestyle=':', linewidth=1)
+plt.axes().set_aspect('equal', 'datalim')
+plt.show()
